0a11a3bfa00992b1e00b233e56df3513b6fb49bc
Kevin Lannen <kevin@luci.com>
2022-11-02 15:29:04 -0600

    Fix incompletee powerline freq enum
    Adapted from https://github.com/lucimobility/meta-intel-realsense/blob/master/recipes-kernel/linux/files/Bug-fix-for-incomplete-PowerLineFrequency-enumeration_5.15.patch

--- drivers/media/usb/uvc/uvc_ctrl.c
+++ drivers/media/usb/uvc/uvc_ctrl.c
@@ -361,6 +361,7 @@ static const struct uvc_menu_info power_line_frequency_controls[] = {
 	{ 0, "Disabled" },
 	{ 1, "50 Hz" },
 	{ 2, "60 Hz" },
+	{ 3, "Auto" },
 };
 
 static const struct uvc_menu_info exposure_auto_controls[] = {
